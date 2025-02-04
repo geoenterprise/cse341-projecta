4dfe4f6f85d32dadae174892a80faa0d2bcb89db
geoenterprise <jovannyrey@hotmail.com>
2025-02-03 20:38:43 -0500

    fixed return issue

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,6 +1,6 @@
 const isAuthenticated = (req, res, next) => {
   if (req.session.user === undefined) {
-    res.status(401).json('You do not have access.');
+    return res.status(401).json('You do not have access.');
   }
   next();
 };
